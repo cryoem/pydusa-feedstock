c07d57e708ba74de8410f47a93ad03211a1af9d6
shadow_walker <shadowwalkersb@gmail.com>
2022-01-19 09:21:59 -0600

    mpi_comm_rank, mpi_comm_size

--- recipe/mpi_test.py
+++ recipe/mpi_test.py
@@ -4,4 +4,14 @@ from mpi import *
 
 mpi_init(0, [])
 
+proc  = mpi_comm_rank(MPI_COMM_WORLD)
+nproc = mpi_comm_size(MPI_COMM_WORLD)
+
+print(f"Process {proc}/{nproc}")
+
+mpi_barrier(MPI_COMM_WORLD)
 mpi_finalize()
+
+if proc == 0:
+	print("\ndone\n")
+	print("If you didn't see any errors above, then the test was a success.\n")
